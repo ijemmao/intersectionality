c58743e0cd0b1ec841de7610b4f892c73696d39d
ijemmao <ijemmao>
2018-11-16 14:15:37 -0500

    added checkbox

--- src/components/card.js
+++ src/components/card.js
@@ -13,6 +13,7 @@ export default class Card extends Component {
         <div className="card-header">
           <h2 className="term">{this.props.term}</h2>
           <h3 className="type">{this.props.type}</h3>
+          <input className="checkbox" type="checkbox" />
         </div>
         <h5 className="term-definition">{this.props.definition}</h5>
       </div>
