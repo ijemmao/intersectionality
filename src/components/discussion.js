6b50139d0dbe01772a197cbb159c927dad88c7c9
ijemmao <ijemmao>
2018-11-17 14:11:10 -0500

    able to add new comments

--- src/components/discussion.js
+++ src/components/discussion.js
@@ -6,6 +6,7 @@ export default class Discussion extends Component {
   constructor(props) {
     super(props);
     this.state= {
+      comment: '',
       comments: [{ text: 'this is a comment', votes: 0 }],
     };
   }
@@ -16,13 +17,26 @@ export default class Discussion extends Component {
     })
   }
 
+  handleCommentInput = (e) => {
+    this.setState({ comment: e.target.value });
+  }
+
+  handleNewComment = () => {
+    if (this.state.comment.length > 5) {
+      let newComment = { text: this.state.comment, vote: 0 };
+      let comments = this.state.comments;
+      comments.push(newComment);
+      this.setState({ comment: '', comments: comments });
+    }
+  }
+
   render() {
     return (
       <div className="discussion-container">
         <h1>Discussion</h1>
         <div className="input-container">
-          <textarea rows="3" placeholder="Share your thoughts" className="discussion-text-input" />
-          <button type="submit" className="submit-comment">Submit</button>
+          <textarea rows="3" placeholder="Share your thoughts" className="discussion-text-input" value={this.state.comment} onChange={this.handleCommentInput} />
+          <button type="submit" className="submit-comment" onClick={this.handleNewComment}>Submit</button>
         </div>
         {this.renderComments()}
       </div>
