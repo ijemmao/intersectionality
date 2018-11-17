9ef9a938b55d720e07c82065c3e43472c9be4ebc
ijemmao <ijemmao>
2018-11-16 20:25:21 -0500

    able to move to detailed page

--- src/components/term.js
+++ src/components/term.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as NavLink } from 'react-router-dom';
 import terms from '../actions/terms';
 import './../styles/term.css';
 
@@ -16,6 +15,10 @@ export default class Term extends Component {
     }
   }
 
+  handleDetail = () => {
+    window.location = `term/${this.props.id}`;
+  }
+
   handleSelection = (e) => {
     let data = Object.assign({ uid: this.props.uid, id: this.props.id, checked: e.target.checked }, this.props.term);
     terms.updateTerm(data);
@@ -23,7 +26,7 @@ export default class Term extends Component {
 
   render() {
     return (
-      <div className="card">
+      <div className="card" onClick={this.handleDetail}>
           <input id={this.props.id} className="checkbox" type="checkbox" onClick={this.handleSelection} />
           <div className="card-header">
             <h2 className="term">{this.props.term.term}</h2>
