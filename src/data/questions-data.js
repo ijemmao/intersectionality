8c60e1f17dbc6f1f48872cf563ba631660d9cc07
ijemmao <ijemmao>
2018-11-23 12:59:54 -0800

    added temp question data file

---
+++ src/data/questions-data.js
@@ -0,0 +1,23 @@
+const tempQuestions = [
+  'What does #MeToo mean to you?',
+  'How does one\'s identity come into play when talking about sexual harassment or assault?',
+  'What are your opinions on how institutions like Dartmouth handle sexual asssult?',
+  'Do you think there\'s a group of people who bear the responsiblity and power to end sexual harassment?',
+  'How important do you think it is for Tarana Burke to lead this movement?',
+  'Thinking of the theme visibility and invisibility, how do you think the movement plays into this concept?',
+  'How did race and gender interplay with one another concerning the Anita Hill and Clarence Thomas case?',
+  'What effects do we see today that stem from the actions in the late 20th century?',
+  'Do you think there\'s a repetitive cycle in place when talking about sexual harassment?',
+  'What were some of your preconceived notions or thoughts on the #MeToo movement before engaging in any type of discussion?',
+  'How do we balance a systemic and societal issue when we identify individual perpetrators?',
+  'How does one\'s culture and religion come into play when discussion sexual harassment or assault?',
+  'Do you think there are fundamental differences between people\'s culture and feminism or similar groups?',
+  'When thinking about institutions that are centuries old, what effects do you see in its timeline or history?',
+  'How does the #MeToo movement\'s reach and affect change when concerning women of color, low-income workers, or queer people?',
+  'Do you have any concerns about the movement?',
+  'How do you see any parts of your identity fitting into or being affected by the #MeToo movement?',
+  'Have there been any parts of your life while growing up that has made it difficult to see eye-to-eye with the movement?',
+  'Do you think it\'s possible to be silenced by the movement? If so, how does the movement end up doing so?',
+];
+
+export default { tempQuestions };
